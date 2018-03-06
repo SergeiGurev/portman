a78bff39a5a909ed9ab1dddfae158bb7f7aa80d8
SergeiGurev <sergeigurev@gmail.com>
2018-03-06 10:54:50 +0500

    checkbox fix

--- views/js/main.js
+++ views/js/main.js
@@ -1,9 +1,5 @@
 'use strict';
 
-function cb() {
-  console.log('sent');
-}
-
 function ajax(address, data, cb) {
   var oReq = new XMLHttpRequest();
   oReq.onload = cb;
@@ -18,13 +14,20 @@ var forms = document.querySelectorAll('form');
 for (var i = 0; i < forms.length; i++) {
   forms[i].addEventListener('submit', function(e) {
     e.preventDefault();
-    var inputs = this.querySelectorAll('input');
+    var inputs = this.querySelectorAll('input[type="text"]');
+    var checkbox = this.querySelector('input[type="checkbox"]');
     var values = {};
+    if (checkbox) {
+      values[checkbox.getAttribute('name')] = checkbox.checked;
+    }
     for (var j = 0; j < inputs.length; j++) {
       values[inputs[j].getAttribute('name')] = inputs[j].value;
     }
 
-    console.log(JSON.stringify(values));
     ajax('/test', JSON.stringify(values), cb);
+
+    function cb() {
+      console.log('sent:', JSON.stringify(values));
+    }
   })
 }
